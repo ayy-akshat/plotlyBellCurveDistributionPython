65395c25f667bf1a3d7e6e36e227438f13fe0ce7
Akze <57077182+ayy-akshat@users.noreply.github.com>
2022-01-01 18:58:52 -0800

    Add files via upload

---
+++ bc.py
@@ -0,0 +1,9 @@
+import random
+import plotly.express as px
+import plotly.figure_factory as ff
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+graph = ff.create_distplot([df["Avg Rating"].tolist()], ["Company Rating Distribution"], show_hist=False)
+graph.show()
